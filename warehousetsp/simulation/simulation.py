d55a35a8e814e19c52b441439f10fd4a6eaee5a5
coleandreoli <coleandreoli@gmail.com>
2025-03-12 13:31:22 +0100

    v1

--- warehousetsp/simulation/simulation.py
+++ warehousetsp/simulation/simulation.py
@@ -6,9 +6,7 @@ class Simulation:
         self.wh = warehouse
 
     def rm_stock(self, name, bin, qty):
-        self.wh.loc[
-            (self.wh["bin"] == bin) & (self.wh["item"] == name), "qty"
-        ] -= qty
+        self.wh.loc[(self.wh["bin"] == bin) & (self.wh["item"] == name), "qty"] -= qty
 
     def get_qty(self, name, bin):
         return int(
@@ -16,13 +14,14 @@ class Simulation:
                 (self.wh["bin"] == bin) & (self.wh["item"] == name), "qty"
             ].values[0]
         )
+
     def get_bins(self, name):
         return self.wh[self.wh["item"] == name]["bin"].to_list()
-    
+
     def get_lowest_bin(self, name):
         # Filter the DataFrame to only include rows for the specified item
         item_bins = self.wh[self.wh["item"] == name]
-        
+
         # If there are no bins for this item, return None or an appropriate value
         if item_bins.empty:
             return None
@@ -30,11 +29,11 @@ class Simulation:
         # Find the row with the smallest 'qty' and return its 'bin' value
         lowest_bin = item_bins.sort_values("qty").iloc[0]["bin"]
         return int(lowest_bin)
-    
+
     def get_highest_bin(self, name):
         # Filter the DataFrame for the specified item.
         item_bins = self.wh[self.wh["item"] == name]
-        
+
         # If no bins are found for the item, return None.
         if item_bins.empty:
             return None
@@ -42,4 +41,3 @@ class Simulation:
         # Sort by 'qty' in descending order and take the first row.
         highest_bin = item_bins.sort_values("qty", ascending=False).iloc[0]["bin"]
         return int(highest_bin)
-
